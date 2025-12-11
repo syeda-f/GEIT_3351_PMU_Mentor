b6bb224ece1f25b8ca94945b6c8197a8f13f4c08
Zunairah-ikhan <zunairah.ik@gmail.com>
2025-12-11 21:34:40 +0300

    Update and rename account_reation.py to account_creation.py

---
+++ account_creation.py
@@ -0,0 +1,15 @@
+#using python dictionaries for making dummy databases
+
+#dummy database of all active PMU student emails and their passwords
+students = [
+  {"Name": "Daman Arshad", "email": "202300481@pmu.edu.sa", "password": "damDam##000"},
+  {"Name": "Syeda Fatima", "email": "202300228@pmu.edu.sa", "password": "syedI2004!!"},
+  {"Name": "Zainab Fatima", "email": "202300994@pmu.edu.sa", "password": "ZaZa67%67%"},
+  {"Name": "Zunairah Khan", "email": "202300994@pmu.edu.sa", "password": "zunZun123#"}
+]
+
+#dummy database of all active mentors
+mentors = [
+  {"Name": "Zainab Fatima",  },
+]
+
